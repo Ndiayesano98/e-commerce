f3f5d7877b921fadc2d2c8f31f7ca2edfc953140
Ndiayesano98 <yankhobasano05@gmail.com>
2024-09-10 16:04:59 +0200

    Mythirdcommit

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './component/Navbar';
 import Home from './component/Home';
+import Products from './component/Products';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <Products/>
     </div>
   );
 }
--- ecommerce/src/component/Products.jsx
+++ ecommerce/src/component/Products.jsx
@@ -1,120 +1,83 @@
-import { useEffect } from "react";
-
-inmport React, { use state, use useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Products = () => {
-
     const [data, setData] = useState([]);
-    const [filter, setFilter] = useState(data);
+    const [filter, setFilter] = useState([]);
     const [loading, setLoading] = useState(false);
-    let componentmounted = true;
 
     useEffect(() => {
+        let componentMounted = true;
+
         const getProducts = async () => {
             setLoading(true);
-            const reponse = await fetch("https://fakestoreapi.com/products");
-            if (componentmounted) {
-                setData(await response.clone());
-                setFilter(await response.json());
-                setLoading(false);
-                console.log(filter)
+            try {
+                const response = await fetch("https://fakestoreapi.com/products");
+                if (componentMounted) {
+                    const products = await response.json();
+                    setData(products);
+                    setFilter(products);
+                }
+            } catch (error) {
+                console.error("Error fetching products:", error);
+            } finally {
+                if (componentMounted) {
+                    setLoading(false);
+                }
             }
+        };
 
-            return () => {
-                componentmounted = false;
-            }
+        getProducts();
 
-       )
-}
+        return () => {
+            componentMounted = false;
+        };
+    }, []);
 
+    const Loading = () => (
+        <div>Loading....</div>
+    );
 
-                    }
-
-const Loading = () => {
-    return (
-        <>
-            Loading....
-        </>
-    )
-}
-const ShowProducts = () => {
-    return (
-        <>
+    const ShowProducts = () => (
+        <div>
             <div className="buttons d-flex justify-content-center mb-5 pb-5">
-                <button className="btn btn-outline-dark me-Z">All</button>
+                <button className="btn btn-outline-dark me-2">All</button>
                 <button className="btn btn-outline-dark me-2">Men's Clothing</button>
                 <button className="btn btn-outline-dark me-2">Women's Clothing</button>
-                <button className="btn btn-outline-dark me-2">Jewelery</button>
-                <button className="btn btn-outline-dark me-2">Electronic</button>
-            </>
-        </div >
-        {
-            filter.map((product) => {
-                return (
-                    <>
-                        <div className="col-md-3">
-                            <div class="card"">
-                            <img src={product.image} class="card-img-top" alt={product.title}>
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">
-                                    ${product.price}
-                                    </p>
-                                    <a href="#"<div class="card" style="width: 18rem;">
-                                    <img src="..." class="card-img-top" alt="...">
-                                    <div class="card-body">
-                                      <h5 class="card-title">Card title</h5>
-                                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    </div>
-                                    <ul class="list-group list-group-flush">
-                                      <li class="list-group-item">An item</li>
-                                      <li class="list-group-item">A second item</li>
-                                      <li class="list-group-item">A third item</li>
-                                    </ul>
-                                    <div class="card-body">
-                                      <a href="#" class="card-link">Card link</a>
-                                      <a href="#" class="card-link">Another link</a>
-                                    </div>
-                                  </div> {
-                                        constructor(parameters) {
-                                            
-                                        }
-                                    }></a>
-                                </div>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">An item</li>
-                                    <li class="list-group-item">A second item</li>
-                                    <li class="list-group-item">A third item</li>
-                                </ul>
-                                <div class="card-body">
-                                    <a href="#" class="card-link">Card link</a>
-                                    <a href="#" class="card-link">Another link</a>
-                                </div>
-                        </div>
-                    </div >
-                </>
-            )
-        }
-        )
-
-        }
-    )
-}
-return (
-    <div>
-        <div className="container my-5 py-5">
-            <div className="row">
-                <div className="col-12 mb-5">
-                    <h1 className="display-6 fw-bolder text-center">Latest Products</h1>
-                    <hr />
-                </div>
+                <button className="btn btn-outline-dark me-2">Jewelry</button>
+                <button className="btn btn-outline-dark me-2">Electronics</button>
             </div>
-            <div className="row justify-content-center">
-                {loading ? <Loading /> : <ShowProducts />}
-
+            <div className="row">
+                {filter.map((product) => (
+                    <div className="col-md-3 mb-4" key={product.id}>
+                        <div className="card h-100 text-center p-4">
+                            <img src={product.image} className="card-img-top" alt={product.title} height="250px" />
+                            <div className="card-body">
+                                <h5 className="card-title">{product.title.substring(0, 12)}</h5>
+                                <p className="card-text">${product.price}</p>
+                                <a href="#" className="btn btn-outline-dark">Buy Now</a>
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
-        <Products />
-    </div>
-);
-}
+    );
+
+    return (
+        <div>
+            <div className="container my-5 py-5">
+                <div className="row">
+                    <div className="col-12 mb-5">
+                        <h1 className="display-6 fw-bolder text-center">Latest Products</h1>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row justify-content-center">
+                    {loading ? <Loading /> : <ShowProducts />}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Products;
